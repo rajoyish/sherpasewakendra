ff5ea25f1f20705374a93803ec9a0ac9cda7be71
Himalayan <hdnpress@gmail.com>
2022-08-02 11:29:42 +0545

    Executive Committee setup with HomeController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,11 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home.executive-committee');
 });
+
+Route::get('/executive-committee', [HomeController::class, 'executiveCommittee'])->name('executive-committee');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
